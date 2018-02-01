37cbcc38fa39a4e3b0140a48bc5ad0a10f97d593
mwnewman <mwnewman@umich.edu>
2018-02-01 14:01:55 -0500

    added the version with keys

--- oauth-twitter-withkeys.py
+++ oauth-twitter-withkeys.py
@@ -14,8 +14,6 @@ oauth = OAuth1Session(client_key,
                           client_secret=client_secret,
                           resource_owner_key=resource_owner_key,
                           resource_owner_secret=resource_owner_secret)
-r = oauth.get(protected_url)
-print (r.text)
 
 protected_url = 'https://api.twitter.com/1.1/search/tweets.json'
 params = {'q':'food'}
